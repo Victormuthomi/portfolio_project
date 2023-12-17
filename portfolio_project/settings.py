cf875485672dbf9d3502c08a5ac37a3bf8a4e56f
victormuthomi <victormuthomi100@gmail.com>
2023-12-18 00:53:35 +0300

    modifield settings.py.

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_o*lrkrhr3uea_w-rf806se==@+^stxus($%yw$(r0udg1vz^f'
+SECRET_KEY = os.environ.GET('DJANG_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', True) == True
 
 ALLOWED_HOSTS = []
 
